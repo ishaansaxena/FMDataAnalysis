ce059398fbe5b5a217987512c294c040d7ba3344
Ishaan <isaxena@purdue.edu>
2018-05-14 12:30:09 +0530

    Add more query functions

--- FMQ/FMQ.py
+++ FMQ/FMQ.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 
 from .abilities import abilities
+import numpy as np
 import pandas
-import numpy
 
 class FMQ:
-    
+
     abilities = abilities
 
     ### Constructors
@@ -41,3 +41,30 @@ class FMQ:
     def get_players_by_name(self, name):
         df = self.df
         return df[df["Name"].str.contains(name, case=False)]
+
+    # Get ability list
+    def get_ability_list(self):
+        return abilities.toDict()
+
+    # Get players with abilities more than threshold: (k, v == ability, lower bound)
+    def filter_by_ability(self, *args, **kwargs):
+        df = self.df
+        for k, v in kwargs.items():
+            df = df.loc[df[k] >= v]
+        return df
+
+    # Get player abilities in range
+    def get_abilities_in_range(self, uid, low, high):
+        df = self.get_player_by_id(uid).select_dtypes(include=[np.number])
+        df = df[self.abilities.all + self.abilities.hidden]
+        r = df.squeeze()
+        return r.loc[r.between(low, high) == True]
+
+    # Get ability average
+    def get_average_ability(self, uid):
+        df = self.get_player_by_id(uid).select_dtypes(include=[np.number])
+        avg = {}
+        for k, v in self.abilities.items():
+            m = df[v].mean(axis=1)
+            avg[k] = m.iat[0]
+        return avg
--- test.py
+++ test.py
@@ -2,9 +2,12 @@ from FMQ import FMQ
 
 def main():
     f = FMQ.FMQ("dataset.csv")
-    df = f.df
-    print(f.get_player_by_id(7458500))
-    print(f.get_players_by_name("lionel"))
+    # print(f.get_players_by_name("lionel"))
+    # print(f.get_player_by_id(7458500))
+    # print(f.get_ability_list())
+    # print(f.filter_by_ability(OneOnOnes=19, AerialAbility=10))
+    # print(f.get_abilities_in_range(7458500, 15, 20))
+    # print(f.get_average_ability(7458500))
 
 if __name__ == '__main__':
     main()
