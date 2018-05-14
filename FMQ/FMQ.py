1717b3cb699e46edb2bd5b04af11408bc00f290c
Ishaan <isaxena@purdue.edu>
2018-05-14 11:29:34 +0530

    Add query methods to FMQ

--- FMQ/FMQ.py
+++ FMQ/FMQ.py
@@ -5,28 +5,39 @@ import pandas
 import numpy
 
 class FMQ:
-
+    
     abilities = abilities
 
-    # Class constructor
+    ### Constructors
+    # Initialize with a specific url
     def __init__(self, url):
         self.url = url
         self.df  = pandas.read_csv(self.url)
 
-    # Return dataset shape
+    ### Summary Methods
+    # Datafram shape
     def shape(self):
-        # Print shape
         return self.df.shape
 
     # Statistical summary
     def summary(self):
         return self.df.describe()
 
-    # Data head
+    # Data head default
     def head(self):
         return self.df.head(5)
 
+    # Data head specific
     def head(self, n):
         return self.df.head(n)
 
-    
+    ### Query methods
+    # Get player by UID
+    def get_player_by_id(self, uid):
+        df = self.df
+        return df.loc[df["UID"] == uid]
+
+    # Get players with name similar to
+    def get_players_by_name(self, name):
+        df = self.df
+        return df[df["Name"].str.contains(name, case=False)]
--- test.py
+++ test.py
@@ -2,9 +2,9 @@ from FMQ import FMQ
 
 def main():
     f = FMQ.FMQ("dataset.csv")
-    print(f.shape())
-    print(f.summary())
-    print([k for k, v in f.abilities.items()])
+    df = f.df
+    print(f.get_player_by_id(7458500))
+    print(f.get_players_by_name("lionel"))
 
 if __name__ == '__main__':
     main()
